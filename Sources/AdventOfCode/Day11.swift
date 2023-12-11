4ecd64b9ce9180bd0421320d3aa444553594aa15
Peter Kovacs <peter@kovax.org>
2023-12-11 13:48:07 -0500

    Day11 Optimization

--- Sources/AdventOfCode/Day11.swift
+++ Sources/AdventOfCode/Day11.swift
@@ -9,18 +9,20 @@ struct Day11: ParsableCommand {
     let emptyRows = Set(0..<grid.size.y).subtracting(galaxies.map(\.y))
     let emptyCols = Set(0..<grid.size.y).subtracting(galaxies.map(\.x))
 
-    func calculate(multiplier: Int) -> Int {
-      galaxies.combinations(ofCount: 2).reduce(0) {
+    func calculate() -> (Int, Int) {
+      galaxies.combinations(ofCount: 2).reduce((0, 0)) {
         let (a, b) = ($1[0], $1[1])
-        return $0 + a.distance(to: b) +
-          (a.y < b.y ? a.y..<b.y : b.y..<a.y).reduce(0) { $0 + (emptyRows.contains($1) ? multiplier : 0) } as Int +
-          (a.x < b.x ? a.x..<b.x : b.x..<a.x).reduce(0) { $0 + (emptyCols.contains($1) ? multiplier : 0) } as Int
+        return (
+          $0.0 + a.distance(to: b), 
+          $0.1 +
+          (a.y < b.y ? a.y..<b.y : b.y..<a.y).reduce(0) { $0 + (emptyRows.contains($1) ? 1 : 0) } as Int +
+          (a.x < b.x ? a.x..<b.x : b.x..<a.x).reduce(0) { $0 + (emptyCols.contains($1) ? 1 : 0) } as Int
+        )
       }
     }
 
-    let part1 = calculate(multiplier: 1)
-    print("Part 1", part1)
-    let part2 = calculate(multiplier: 1_000_000 - 1)
-    print("Part 2", part2)
+    let (distance, crossings) = calculate()
+    print("Part 1", distance + crossings)
+    print("Part 2", distance + (crossings * 999_999))
   }
 }
