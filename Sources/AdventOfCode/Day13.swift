72ec509cd46cf543eb63d87786b301ad840918f1
Peter Kovacs <peter@kovax.org>
2023-12-13 12:58:44 -0500

    Day 13 optimizations

--- Sources/AdventOfCode/Day13.swift
+++ Sources/AdventOfCode/Day13.swift
@@ -1,13 +1,35 @@
 
 import ArgumentParser
+import Parsing
 import Utility
+import Foundation
 
 struct Day13: ParsableCommand {
-  func run() throws {
-    let grids = stdin.split(separator: #/\n\n/#).map {
-      let lines = $0.split(separator: "\n")
-      return Grid.init(lines.joined(), size: .init(x: lines[0].count, y: lines.count))
+  enum Parser {
+    static let grid = Many(into: (.zero, [])) { (partialResult: inout (Coord, [Substring.UTF8View]), i: Substring.UTF8View) in
+      partialResult.0.x = i.count
+      partialResult.0.y += 1
+      partialResult.1.append(i)
+    } element: {
+      Prefix<Substring.UTF8View> { $0 == 46 || $0 == 35 }
+    } separator: {
+      Whitespace(1, .vertical)
+      Peek { Not { Whitespace(1, .vertical) } }
+    }.map {
+      Grid($0.1.joined(), size: $0.0)
     }
+
+    static let parser: some Parsing.Parser<Substring.UTF8View, [Grid<Substring.UTF8View.Element>]> = Many {
+      grid
+    } separator: {
+      Whitespace(2, .vertical)
+    } terminator: {
+      End()
+    }
+  }
+
+  func run() throws {
+    let grids: [Grid<Substring.UTF8View.Element>] = try Parser.parser.parse(stdin)
 
     let part1 = grids.reduce(0) { $0 + $1.mirror(allowed: 0) }
     print("Part 1", part1)
@@ -57,18 +79,24 @@ struct ColMirrorIterator: Sequence, IteratorProtocol {
   }
 }
 
-private extension Grid where Element == Character {
+private extension Grid where Element == Substring.UTF8View.Element {
   func mirror(allowed: Int) -> Int {
     for col in 1..<(size.x) {
       let match = ColMirrorIterator(size: size, left: .init(x: col - 1, y: 0), right: .init(x: col, y: 0))
-        .lazy.filter { self[$0.0] != self[$0.1] }.count
+        .lazy
+        .filter { self[$0.0] != self[$0.1] }
+        .prefix(allowed + 1)
+        .count
 
       if match == allowed { return col }
     }
 
     for row in 1..<(size.y) {
       let match = RowMirrorIterator(size: size, top: .init(x: 0, y: row - 1), bottom: .init(x: 0, y: row))
-        .lazy.filter { self[$0.0] != self[$0.1] }.count
+        .lazy
+        .filter { self[$0.0] != self[$0.1] }
+        .prefix(allowed + 1)
+        .count
 
       if match == allowed { return row * 100 }
     }
